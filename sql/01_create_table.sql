aad649d131e6d24bdaaae8cb35de7a136089a111
Georgean Reyes <greyesf1@miumg.edu.gt>
2026-04-10 18:23:12 -0600

    Create online_retail table with relevant columns

---
+++ sql/01_create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE online_retail (
+    invoice_no TEXT,
+    stock_code TEXT,
+    description TEXT,
+    quantity INTEGER,
+    invoice_date TIMESTAMP,
+    unit_price NUMERIC(10,2),
+    customer_id TEXT,
+    country TEXT
+);
